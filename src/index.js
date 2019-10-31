27e8dfeefcfe75fe369932eb3daa7cd2cf862327
ysc <you@example.com>
2019-10-31 16:46:15 +0800

    初始项目

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './assets/css/reset.css'
+import './assets/js/rem'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
